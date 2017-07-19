aa2b275ca6f2efb46ffd90f990816d97c63e288d
karmenzind <valesail7@gmail.com>
2017-07-19 15:51:00 +0800

    other formattin'

--- showSubPlot.m
+++ showSubPlot.m
@@ -1,9 +1,7 @@
 function [XData, YData] = showSubPlot(h_proc)
-%UNTITLED9 Summary of this function goes here
-%   Detailed explanation goes here
 
 figure
-% subplot 211
+
 h = h_proc; % Draw your doodle here
 hc = get(h,'Children');
 XData = []; YData = [];
@@ -15,16 +13,6 @@ for ii=1:length(hc)
 end
 subplot 
 plot(XData,YData)
-
-% disp(XData);
-% disp(YData);
-% for m = 1:length(XData)
-%     disp XData(m);
-% end
-% for n = 1:length(YData)
-%     YData(n);
-% end
-    
 
 return
 
